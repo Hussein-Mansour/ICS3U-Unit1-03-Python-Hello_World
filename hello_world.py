e575aed39935914763de2c8877c7ae71c4916904
Hussein-Mansour <hussein.mansour@ocsbstudent.ca>
2021-04-25 18:00:30 +0000

    added in hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Hussein Mansour
+# Created on: Sun,Apr25/2020
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
